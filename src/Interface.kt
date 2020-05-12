e63f2a31eac8fd11d74fc2b33e2f37d4a25fef49
Jo Helmuth <johelmuth@gmx.de>
2020-05-12 16:49:45 +0200

    introduce word matcher class

--- src/Interface.kt
+++ src/Interface.kt
@@ -2,16 +2,23 @@ import java.io.File
 
 fun main(arg: Array<String>) {
 
-    var words: List<String> = loadWords()
-
-    var substring: String = "dr"
-    var matchingWords: MutableList<String> = findMatchingWords(words,substring)
-
+    var wm: WordMatcher = WordMatcher()
     val cw: ConsoleWrapper = ConsoleWrapper()
-    cw.switchTerminalMode(rawMode = true)
-    cw.updateAndDisplayState(substring,matchingWords)
-    var c = cw.readNextChar()
-    cw.switchTerminalMode(rawMode = false)
+    var substring: String = ""
+    var matches: MutableList<String> = mutableListOf<String>()
+
+    cw.initConsole()
+
+    for(i in 1..4) {
+        cw.updateAndDisplayState("", matches)
+
+        substring += cw.readNextChar()
+
+        matches = wm.findMatchingWords(words = null, subString = substring)
+    }
+    cw.updateAndDisplayState(substring,matches)
+
+    cw.resetConsole()
 
     //switchTerminalMode(true)
     //eraseScreen()
@@ -28,7 +35,7 @@ class ConsoleWrapper() {
 
     private var cursorPosition: Int = 1
 
-    fun switchTerminalMode(rawMode: Boolean = false): Unit {
+    private fun switchTerminalMode(rawMode: Boolean = false): Unit {
         // switches terminal to normal (line) mode
         var cmd = arrayOf("/bin/sh", "-c", "stty sane </dev/tty")
         if (rawMode)
@@ -37,6 +44,18 @@ class ConsoleWrapper() {
 
         // execute command
         Runtime.getRuntime().exec(cmd).waitFor()
+    }
+
+    fun initConsole(): Unit {
+        switchTerminalMode(rawMode = true)
+        eraseScreen()
+        placeCursor(1,1)
+    }
+
+    fun resetConsole(): Unit {
+        eraseScreen()
+        placeCursor(1,1)
+        switchTerminalMode(rawMode = false)
     }
 
     private fun eraseScreen(): Unit {
@@ -83,22 +102,32 @@ class ConsoleWrapper() {
     }
 }
 
-fun loadWords(): List<String>{
-    val file = File("data/english-nouns.txt")
-    return file.readLines()
-}
+class WordMatcher() {
 
-fun findMatchingWords(words: List<String>, subString: String): MutableList<String> {
-    var matchingWords: MutableList<String> = mutableListOf<String>()
-    val l1: Int = subString.length
-    var l2: Int
-    for (word in words) {
-        l2 = word.length
-        // lazy?
-        // ask for permission or ask for forgiveness?
-        if (l2>=l1 && word.subSequence(0,l1)==subString) {
-            matchingWords.add(word)
-        }
+    var words: List<String> = loadWords()
+
+    private fun loadWords(): List<String> {
+        val file = File("data/english-nouns.txt")
+        return file.readLines()
     }
-    return matchingWords
+
+    fun findMatchingWords(words: List<String>?, subString: String): MutableList<String> {
+        var matchingWords: MutableList<String> = mutableListOf<String>()
+        val l1: Int = subString.length
+        var l2: Int
+        var useWords: List<String> = this.words
+        if (words!=null) {
+            useWords = words
+        }
+
+        for (word in useWords) {
+            l2 = word.length
+            // lazy?
+            // ask for permission or ask for forgiveness?
+            if (l2 >= l1 && word.subSequence(0, l1) == subString) {
+                matchingWords.add(word)
+            }
+        }
+        return matchingWords
+    }
 }
